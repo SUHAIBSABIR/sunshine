3b682171d36ca2425e2423955eb8ab97744d7411
SUHAIB SABIR <suhaibsabir110@gmail.com>
2024-03-26 13:06:39 -0700

    x
    x

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true
 });
+
+
+
+var a = document.querySelector("#page3")
+var b = document.querySelector("#fixed-img")
+
+
+a.addEventListener("mouseenter", function () {
+
+    b.style.display = "block"
+
+})
+
+
+a.addEventListener("mouseleave", function () {
+
+    b.style.display = "none"
+
+})
+
---
+++ fonts/test.js
@@ -0,0 +1,7 @@
+var a = document.querySelector("#img")
+console.log(a)
+var b = a.getAttribute("data-name")
+console.log(b)
+
+var c = a.getAttribute("data-company")
+console.log(c)
